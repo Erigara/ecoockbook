63be441e6407f8cc794a58cb0573385c3c487e8c
Shanin Roman <shanin1000@yandex.ru>
2020-12-02 21:57:30 +0300

    Базовая настройка для локального развертывания

--- ecookbook/settings.py
+++ ecookbook/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ecookbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecookbook',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
---
+++ update_requirements.sh
@@ -0,0 +1,2 @@
+/home/erigara/code/python/projects/ecookbook/venv/bin/python -m pip freeze > requirements.txt
+echo requirements.txt updated successfully!
