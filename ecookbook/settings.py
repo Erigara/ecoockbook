bd98266fccfdef6fc8e8f2a332d5404428dae0fc
Shanin Roman <shanin1000@yandex.ru>
2020-12-03 11:59:34 +0300

    Пользователи

--- ecookbook/settings.py
+++ ecookbook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,8 @@ DATABASES = {
     }
 }
 
+# User Model
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
