111545cbc6f0fe1f6b10668f32b3486dcd4f338e
Shanin Roman <shanin1000@yandex.ru>
2020-12-03 15:50:21 +0300

    Фиксы форматирования

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
-import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+
 from users.utils import avatar_upload_to
+
 
 class User(AbstractUser):
     avatar = models.ImageField(_('avatar'), upload_to=avatar_upload_to, blank=True)
@@ -11,4 +12,3 @@ class User(AbstractUser):
     phone_number = PhoneNumberField(_('phone number'), unique=True)
 
     REQUIRED_FIELDS = AbstractUser.REQUIRED_FIELDS + ['phone_number']
-
