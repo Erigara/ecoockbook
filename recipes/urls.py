33e12b4f15f00d74a21c65f9b7c3520f630a8ff6
Shanin Roman <shanin1000@yandex.ru>
2020-12-20 18:23:51 +0300

    Урлы для обработки компонентов рецепта

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,18 +4,21 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.routers import DefaultRouter
 
-from recipes.views import ChefViewSet, LikedRecipeListView, RecipeListCreateView, RecipeView, LikeView, CategoryViewSet, \
-    InCategoryRecipeListView, RecipeViewSet
+from recipes.views import ChefViewSet, LikeView, CategoryViewSet, \
+    RecipeViewSet, RecipeImageViewSet, StepViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r'chefs', ChefViewSet, basename='chef')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'recipes', RecipeViewSet, basename='recipe')
+router.register(r'recipes/(?P<recipe>[^/.]+)/images', RecipeImageViewSet, basename='recipe-image')
+router.register(r'recipes/(?P<recipe>[^/.]+)/steps', StepViewSet, basename='recipe-step')
+router.register(r'recipes/(?P<recipe>[^/.]+)/products', ProductViewSet, basename='recipe-product')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('recipes/<int:recipe>/likes', LikeView.as_view(), name='likes'),
+    path('recipes/<int:recipe>/likes', LikeView.as_view(), name='recipe-like'),
     # path('likes/recipes', LikedRecipeListView.as_view(), name='recipe-favorites'),
     # path('categories/<int:category>/recipes', InCategoryRecipeListView.as_view(), name='recipe-in-category'),
 
