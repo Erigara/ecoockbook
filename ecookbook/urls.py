1ac2d83e9a5bd7794a271dd78bd599e6a6278cdb
Shanin Roman <shanin1000@yandex.ru>
2021-02-28 17:19:07 +0300

    config urls

--- ecookbook/urls.py
+++ ecookbook/urls.py
@@ -18,14 +18,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+urlpatterns = []
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/cookbook/', include('recipes.urls')),
     path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# last to prevent collisions
+urlpatterns.append(path('', include('frontend.urls')))
