95f1d0bd40b14969c64784faf350d2ee938960ac
Shanin Roman <shanin1000@yandex.ru>
2021-02-28 15:12:11 +0300

    frontend settings

--- ecookbook/settings.py
+++ ecookbook/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = 'static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Media
-MEDIA_ROOT = '/code/media'
+MEDIA_ROOT = 'media/'
 MEDIA_URL = 'media/'
 
 #  Rest framework
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,18 +1,24 @@
+const path = require('path');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
-      }
-    ]
-  }
+    output: {
+        path: path.resolve(__dirname, 'static/frontend'),
+        filename: "main.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    }
 };
