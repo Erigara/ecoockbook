c27d6a8a784e1b8e5dff8ed745b6f1dad6572c7a
Shanin Roman <shanin1000@yandex.ru>
2021-03-01 22:12:56 +0300

    add redirects

--- frontend/src/components/ProfileMenu.js
+++ frontend/src/components/ProfileMenu.js
@@ -2,22 +2,25 @@ import React, {useEffect, useState} from "react";
 import {Avatar, Menu} from 'antd';
 import {UserOutlined, LogoutOutlined} from '@ant-design/icons';
 import {logout} from "../api/usersApi";
+import {Link, useHistory} from "react-router-dom";
 
 const {SubMenu} = Menu;
 
 export default function ProfileMenu(props = {}) {
+    const history = useHistory();
     const [display, setDisplay] = useState('block');
     const {user, setUser} = props;
 
     const handleClick = (e) => {
         switch (e.key) {
             case "profile":
-                //
+                history.push("/profile");
                 break;
             case "logout":
                 logout().then(res => {
                     if (res.status === 200) {
                         setUser(null);
+                        history.push("/");
                     }
                 }).catch(err => {
                     console.log(err);
